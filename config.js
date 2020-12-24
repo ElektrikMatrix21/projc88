36fe4347d53c6c7545b6b49e4ebeb3d724c5fc73
ElektrikMatrix21 <50866167+ElektrikMatrix21@users.noreply.github.com>
2020-12-24 11:50:53 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase';
+require('@firebase/firestore')
+
+var firebaseConfig = {
+  apiKey: "AIzaSyBJ6dfWslS8enUfzQjPJ1ywj4UWvXs4UsY",
+  authDomain: "barterapp-c12d6.firebaseapp.com",
+  databaseURL: "https://barterapp-c12d6.firebaseio.com",
+  projectId: "barterapp-c12d6",
+  storageBucket: "barterapp-c12d6.appspot.com",
+  messagingSenderId: "500817200788",
+  appId: "1:500817200788:web:be738e447d6d7cc3f69681",
+  measurementId: "G-ZQ3L38J3H3"
+  };
+  // Initialize Firebase
+
+  firebase.initializeApp(firebaseConfig);
+
+  export default firebase.firestore();
